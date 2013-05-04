976b6b5e679ec66c7673b51d08a62ed3c40a91d4
Christoffer Artmann <Artgaard@gmail.com>
2013-05-04 16:19:14 +0200

    Session config

--- app/models/Users.php
+++ app/models/Users.php
@@ -6,6 +6,7 @@
 		protected $_schema = array(
 		    'id'  => array('type' => 'id'), // required for Mongo
 		    'email' => array('type' => 'string', 'default' => 'Moe', 'null' => false),
+		    'name' => array('type' => 'string', 'null' => false)
 		    'password' => array('type' => 'string', 'default' => 'bar', 'null' => false),
 		);
 

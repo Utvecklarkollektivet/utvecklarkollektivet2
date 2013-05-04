d71e5348bd570efa4cc7e7cc5a859919c4143fa7
Christoffer Artmann <Artgaard@gmail.com>
2013-05-04 19:04:43 +0200

    Added Role Users relations

--- app/models/Users.php
+++ app/models/Users.php
@@ -8,8 +8,14 @@
 		    'email' => array('type' => 'string', 'default' => 'Moe', 'null' => false),
 		    'name' => array('type' => 'string', 'null' => false),
 		    'password' => array('type' => 'string', 'default' => 'bar', 'null' => false),
+		    'role_id' => array("type" => "integer", "default" => 1, "null" => false),
 		);
+
+		public $belongsTo = array("Roles");
 
 	}
 
 ?>
+
+
+

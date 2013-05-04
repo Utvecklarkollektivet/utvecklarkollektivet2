021962eb71036f1e57be7e5aa16d85bd6f9e5d77
Filip Jansson <info@filipjansson.se>
2013-05-04 19:20:45 +0200

    Json

--- app/models/Whiteboards.php
+++ app/models/Whiteboards.php
@@ -3,6 +3,13 @@
 namespace app\models;
 
 class Whiteboards extends \lithium\data\Model {
+
+    protected $_schema = array(
+    'id'  => array('type' => 'id'), // required for Mongo
+    'title' => array('type' => 'string', 'default' => 'Moe', 'null' => false),
+    'sign' => array('type' => 'string', 'default' => 'bar', 'null' => false),
+    'age'  => array('type' => 'integer', 'default' => 0, 'null' => false)
+    );
 }
 
 ?>
--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -11,8 +11,8 @@
 <head>
 	<?php echo $this->html->charset();?>
 	<title>Application &gt; <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('debug', 'lithium')); ?>
-	<?php echo $this->scripts(); ?>
+	<?php echo $this->html->style(array('debug', 'lithium', 'foundation')); ?>
+	<?php echo $this->scripts(array('foundation.min.js')); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
 <body class="app">
